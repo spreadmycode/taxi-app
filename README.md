17f439f47b2117f8a6324fb65ec268d93c7399ae
spreadmycode <spreadmycode@outlook.com>
2022-12-14 20:45:25 -0800

    create: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Taxi Claim-Refund App
---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import { Navbar } from "flowbite-react";
+import { Dropdown } from "flowbite-react";
+import { Alert } from "flowbite-react";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Taxi App</title>
+        <meta name="description" content="Taxi App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar fluid={true} rounded={true}>
+        <Navbar.Brand href="https://flowbite.com/">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Flowbite
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="/navbars" active={true}>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/navbars">About</Navbar.Link>
+          <Navbar.Link href="/navbars">Services</Navbar.Link>
+          <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+          <Navbar.Link href="/navbars">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <div>
+        <Alert color="info">Alert!</Alert>
+        <Dropdown label="Dropdown button">
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+      </div>
+    </div>
+  );
+}
