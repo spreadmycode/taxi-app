4ba9c623284fcaa8806226f3d56fe27debdedc35
spreadmycode <spreadmycode@outlook.com>
2022-12-21 13:23:43 -0800

    stage: hero padding

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -121,8 +121,8 @@ const HeroSection = () => {
         </div>
       </section> */}
       <section className="bg-white dark:bg-gray-900">
-        <div className="max-w-screen-xl mx-auto md:px-20">
-          <div className="grid lg:grid-cols-12 lg:gap-8 xl:gap-0 px-4 py-8 lg:py-40">
+        <div className="max-w-screen-xl mx-auto px-4 md:px-20 py-8 lg:py-40">
+          <div className="grid lg:grid-cols-12 lg:gap-8 xl:gap-0">
             <div className="mr-auto place-self-center lg:col-span-7">
               <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                 Your £624 tax refund starts here
