597163af7e779d5fa2468251cd1dee19d2b6844a
spreadmycode <spreadmycode@outlook.com>
2022-12-21 09:13:52 -0800

    fix: step alert

--- components/StepAlert.tsx
+++ components/StepAlert.tsx
@@ -10,7 +10,7 @@ const StepAlert = ({ step }: Props) => {
       {step == STEP.LAST_THING && (
         <div className="w-full pt-4 mx-auto lg:pt-8">
           <div
-            className="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
+            className="p-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
             role="alert"
           >
             Only two steps left
@@ -20,7 +20,7 @@ const StepAlert = ({ step }: Props) => {
       {step == STEP.THANK_YOU && (
         <div className="w-full pt-4 mx-auto lg:pt-8">
           <div
-            className="p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800"
+            className="p-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800"
             role="alert"
           >
             ⚠️ This is the last question
