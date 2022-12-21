47b6039659ef01a638231dbc96a5312d515867c6
spreadmycode <spreadmycode@outlook.com>
2022-12-21 11:49:28 -0800

    feat: hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -28,8 +28,8 @@ const HeroSection = () => {
 
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7">
+      <div className="max-w-screen-xl py-8 mx-auto lg:flex gap-2">
+        <div className="mx-auto md:w-[42rem] px-4 md:px-8 xl:px-0">
           <h1 className="max-w-2xl mb-6 lg:mb-10 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl xl:text-6xl dark:text-white">
             Your&nbsp;
             <span className="text-primary-600 dark:text-primary-500">£</span>
@@ -101,7 +101,7 @@ const HeroSection = () => {
             </ul>
           </div>
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div className="hidden w-full max-w-md lg:h-auto lg:block">
           <div className="rounded-lg shadow-lg dark:hidden h-max">
             <img
               className=""
@@ -118,7 +118,7 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      <div className="grid max-w-screen-xl grid-cols-2 gap-8 px-4 pb-8 mx-auto text-gray-500 lg:pb-16 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+      {/* <div className="grid max-w-screen-xl grid-cols-2 gap-8 px-4 pb-8 mx-auto text-gray-500 lg:pb-16 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
         <a href="#" className="flex justify-center">
           <svg
             className="h-8 hover:text-gray-900 dark:hover:text-white"
@@ -269,7 +269,7 @@ const HeroSection = () => {
             />
           </svg>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
