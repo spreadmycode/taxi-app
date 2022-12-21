e4867bdad87ec9a552e52d26d757d856cdbd3c4b
spreadmycode <spreadmycode@outlook.com>
2022-12-20 21:36:21 -0800

    fix: icon centered

--- components/steps/Step3-SignComplete.tsx
+++ components/steps/Step3-SignComplete.tsx
@@ -8,7 +8,7 @@ const SignComplete = () => {
           return (
             <li key={index} className="flex justify-start items-start">
               <svg
-                className="w-4 h-4 mt-1.5 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
+                className="w-4 h-4 mt-[5px] mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
